8c27a4bbb399ca4bc7afb2a9542741d87ce63408
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-13 23:56:17 +0600

    Initial Commit

---
+++ ChatGPTDemo/ChatGPTDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTDemoApp.swift
+//  ChatGPTDemo
+//
+//  Created by Joynal Abedin on 13/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGPTDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatGPTDemo
+//
+//  Created by Joynal Abedin on 13/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
